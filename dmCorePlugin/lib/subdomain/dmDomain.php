47c0ebac04358175d5cfeedf5e6c03bb1dd5a867
etherkye <kye@pimlicoplumbers.com>
2011-01-19 13:18:52 +0000

    Added dmDomain service.  Editing several modules to use the new service to calculate the links from the frontend.  If the site_url is left blank in admin then the service takes the current URL for making all the links.

---
+++ dmCorePlugin/lib/subdomain/dmDomain.php
@@ -0,0 +1,120 @@
+<?php
+
+class dmDomain extends dmConfigurable {
+
+    protected
+    $domain,
+    $subdomain;
+
+    public function __construct(array $options = array()) {
+        $this->initialize($options);
+    }
+
+    protected function initialize(array $options = array()) {
+        $this->domain = dmConfig::get('site_url');
+        if (empty($this->domain)) {
+            $this->domain = $this->generateDomain($_SERVER['SERVER_NAME']);
+        }
+        $this->subdomain = $this->generateSubDomain($_SERVER['SERVER_NAME'], $this->domain);
+
+        $this->configure($options);
+    }
+
+    private function generateDomain($domainb) {
+        $bits = explode('/', $domainb);
+        if ($bits[0] == 'http:' || $bits[0] == 'https:') {
+            $domainb = $bits[2];
+        } else {
+            $domainb = $bits[0];
+        }
+        unset($bits);
+        $bits = explode('.', $domainb);
+        $idz = 0;
+
+        while (isset($bits[$idz])) {
+            $idz+=1;
+        }
+
+        $idz-=3;
+        $idy = 0;
+        while ($idy < $idz) {
+            unset($bits[$idy]);
+            $idy+=1;
+        }
+        $part = array();
+        foreach ($bits AS $bit) {
+            $part[] = $bit;
+        }
+        unset($bit);
+        unset($bits);
+        $domainb = '';
+        if (strlen($part[1]) > 3) {
+            unset($part[0]);
+        }
+        foreach ($part AS $bit) {
+            $domainb.=$bit . '.';
+        }
+        unset($bit);
+        return preg_replace('/(.*)\./', '$1', $domainb);
+    }
+
+    private function generateSubDomain($domainb, $domain) {
+        $bits = explode('/', $domainb);
+        if ($bits[0] == 'http:' || $bits[0] == 'https:') {
+            $domainb = $bits[2];
+        } else {
+            $domainb = $bits[0];
+        }
+        $bits = explode($domain, $domainb);
+        return trim($bits[0], '.');
+    }
+
+    private  function isSameSubdomain($subdomain) {
+        if ($subdomain == "DEFAULT") {
+            $subdomain = dmConfig::get('site_subdomain_default');
+        }
+        return $subdomain == $this->subdomain;
+    }
+    private function checkPrefix($prefix){
+        if(substr($prefix,0,1) == '/'){
+            return $prefix;
+        }
+        return '/'.$prefix;
+    }
+
+    public function getDomain(){
+        return $this->domain;
+    }
+    public function printDomain(){
+        return $this->domain;
+    }
+    public function getSubdomain(){
+        if ($this->subdomain == dmConfig::get('site_subdomain_default')) {
+            return "DEFAULT";
+        }
+        return $this->subdomain;
+    }
+
+    public function printSubdomain($subdomain){
+        if ($subdomain == "DEFAULT") {
+            $subdomain = dmConfig::get('site_subdomain_default');
+        }
+
+        return (!empty($subdomain)?$subdomain.".":"");
+    }
+
+    public function returnLink($prefix, $slug, $subdomain){
+        if($this->isSameSubdomain($subdomain)){
+            $baseHref = $this->checkPrefix($prefix).($slug ? '/'.$slug : '');
+        }else{
+            $baseHref = "http://" . $this->printSubdomain($subdomain) . $this->printDomain() . $this->checkPrefix($prefix).($slug ? '/'.$slug : '');
+        }
+
+        if(empty($baseHref))
+        {
+          $baseHref = '/';
+        }
+
+        return $baseHref;
+    }
+}
