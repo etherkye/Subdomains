3afef9d50a18a2f09283128599d72955e556eb0f
etherkye <kye@pimlicoplumbers.com>
2011-09-30 17:18:46 +0100

    Bug in cookies. Temp fix.

--- dmCorePlugin/lib/session/dmSessionStorage.class.php
+++ dmCorePlugin/lib/session/dmSessionStorage.class.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * This file is part of the dmCorePlugin package.
  * (c) 2011 Diem project
@@ -14,18 +15,54 @@
  * @see sfSessionStorage
  *
  */
-class dmSessionStorage extends sfSessionStorage
-{
+class dmSessionStorage extends sfSessionStorage {
 
   protected
   $serviceContainer;
 
-  public function initialize($options = null)
-  {
-    if(!isset($options['session_cookie_domain']) || $options['session_cookie_domain'] == ''){
-      $options['session_cookie_domain']  = ((isset($_SERVER['SERVER_NAME']))?'.'.$_SERVER['SERVER_NAME']:'');
+  public function initialize($options = null) {
+    if (!isset($options['session_cookie_domain']) || $options['session_cookie_domain'] == '') {
+      $options['session_cookie_domain'] = ((isset($_SERVER['SERVER_NAME'])) ? '.' . $this->generateDomain($_SERVER['SERVER_NAME']) : '');
     }
     parent::initialize($options);
+  }
+
+  private function generateDomain($domainb) {
+    $bits = explode('/', $domainb);
+    if ($bits[0] == 'http:' || $bits[0] == 'https:') {
+      $domainb = $bits[2];
+    } else {
+      $domainb = $bits[0];
+    }
+    unset($bits);
+    $bits = explode('.', $domainb);
+    $idz = 0;
+
+    while (isset($bits[$idz])) {
+      $idz+=1;
+    }
+
+    $idz-=3;
+    $idy = 0;
+    while ($idy < $idz) {
+      unset($bits[$idy]);
+      $idy+=1;
+    }
+    $part = array();
+    foreach ($bits AS $bit) {
+      $part[] = $bit;
+    }
+    unset($bit);
+    unset($bits);
+    $domainb = '';
+    if (isset($part[1]) && strlen($part[1]) > 3) {
+      unset($part[0]);
+    }
+    foreach ($part AS $bit) {
+      $domainb.=$bit . '.';
+    }
+    unset($bit);
+    return preg_replace('/(.*)\./', '$1', $domainb);
   }
 
 }
